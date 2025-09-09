0b667ee850245f829e427b23f14ab2cc181fdecf
xogur <snsk656@naver.com>
2025-09-09 19:58:00 +0900

    스프링 데이터JPA 페이징과 정렬

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,7 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.result.ModelResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
@@ -158,6 +163,39 @@ class MemberRepositoryTest {
 
         List<Member> aaa = memberRepository.findListByUsername ("AAA");
 
+    }
+
+    @Test
+    public void paging() {
+        memberRepository.save (new Member ("member1", 10));
+        memberRepository.save (new Member ("member2", 10));
+        memberRepository.save (new Member ("member3", 10));
+        memberRepository.save (new Member ("member4", 10));
+        memberRepository.save (new Member ("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of (0, 3, Sort.by (Sort.Direction.DESC, "username"));
+
+        Page<Member> page = memberRepository.findByAge (age, pageRequest);
+
+        Page<MemberDto> toMap = page.map (member -> new MemberDto (member.getId (), member.getUsername (), null));
+
+        List<Member> content = page.getContent ();
+        long totalElements = page.getTotalElements ();
+
+//        for (Member member : content) {
+//            System.out.println ("member = " + member);
+//
+//        }
+//        System.out.println ("totalElements = " + totalElements);
+
+        assertThat (content.size ()).isEqualTo (3);
+        assertThat (page.getTotalElements ()).isEqualTo (5);
+        assertThat (page.getNumber ()).isEqualTo (0);
+        assertThat (page.getTotalPages ()).isEqualTo (2);
+        assertThat (page.isFirst ()).isTrue ();
+        assertThat (page.hasNext ()).isTrue ();
+        System.out.println (page);
     }
 
 }
