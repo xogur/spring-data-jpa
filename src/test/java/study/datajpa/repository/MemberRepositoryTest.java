e1cf14e5e46c59c427b3e7ed4e6e12196b0bc31a
xogur <snsk656@naver.com>
2025-09-10 14:53:30 +0900

    Hint & Lock

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -242,4 +242,27 @@ class MemberRepositoryTest {
         }
     }
 
+    @Test
+    public void queryHint() {
+        Member member1 = new Member ("member1", 10);
+        memberRepository.save (member1);
+        em.flush ();
+        em.clear ();
+
+        Member findMember = memberRepository.findReadOnlyByUsername ("member1");
+        findMember.setUsername ("member2");
+
+        em.flush ();
+    }
+
+    @Test
+    public void Lock() {
+        Member member1 = new Member ("member1", 10);
+        memberRepository.save (member1);
+        em.flush ();
+        em.clear ();
+
+        List<Member> result = memberRepository.findLockByUsername ("member1");
+
+    }
 }
