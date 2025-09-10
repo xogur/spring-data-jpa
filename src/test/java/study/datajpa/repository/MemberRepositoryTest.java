33746e9368c94d331b8e37307b30945ded8efef5
xogur <snsk656@naver.com>
2025-09-10 15:28:21 +0900

    사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -37,6 +37,9 @@ class MemberRepositoryTest {
     @PersistenceContext
     EntityManager em;
 
+    @Autowired
+    MemberQueryRepository memberQueryRepository;
+
     @Test
     public void testMember() {
         Member member = new Member ("memberA");
@@ -264,5 +267,10 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findLockByUsername ("member1");
 
+    }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom ();
     }
 }
