02c663f2a7a132bb87d56794c65f5bae672ac42e
xogur <snsk656@naver.com>
2025-09-11 16:53:20 +0900

    final

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -322,4 +322,26 @@ class MemberRepositoryTest {
         }
 
     }
+
+    @Test
+    public void nativeQuery() {
+        Team teamA = new Team ("teamA");
+        em.persist (teamA);
+
+        Member m1 = new Member ("m1", 0, teamA);
+        Member m2 = new Member ("m2", 0, teamA);
+
+        em.persist (m1);
+        em.persist (m2);
+
+        em.flush ();
+        em.clear ();
+
+        Page<MemberProjection> result = memberRepository.findByNativeProjection (PageRequest.of (0, 10));
+        List<MemberProjection> content = result.getContent ();
+        for (MemberProjection memberProjection : content) {
+            System.out.println ("memberProjection.getUsername () = " + memberProjection.getUsername ());
+            System.out.println ("memberProjection.getTeamName () = " + memberProjection.getTeamName ());
+        }
+    }
 }
